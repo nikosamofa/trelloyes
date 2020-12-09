4cdac3553cb1943116d80457ed81346019854b0e
Nikos Amofa <nikosbright@gmail.com>
2020-12-09 17:01:21 -0500

    added component tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,27 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
+import ReactDOM from 'react-dom'
+import Card from './Card';
+import List from './List'
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+
+describe('Card component', ()=>{
+  it('should render without crashing',()=>{
+    const div = document.createElement('div');
+
+    ReactDOM.render(<Card />, div);
+
+    ReactDOM.unmountComponentAtNode(div);
+  } );
+
+  //snapshot test 
+  it('should render UI as expected',()=>{
+    const tree = renderer.create(<Card
+      key={card.id}
+      title={card.title}
+      content={card.content}
+    />).toJSON();
+
+    expect(tree).toMatchSnapshot();
+  })
+})
